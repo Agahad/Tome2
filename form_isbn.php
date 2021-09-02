a037951a3493bb3444551833926da7855108dea1
Alexandre Obert <aclhobert@gmail.com>
2021-09-02 16:00:33 +0200

    création formulaire isbn

---
+++ form_isbn.php
@@ -0,0 +1,5 @@
+<h2>Recherche info livre avec code ISBN</h2>
+        <form action="infoslivres.php" method="post">
+			<p><label for="isbn"> ISBN : <input type="text" id="isbn" name="isbn"/></p>
+			<p><input type="submit" value="Envoi"></p>
+		</form>
